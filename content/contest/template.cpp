78e1747c11981794476238027d0b5ff8db69cfae
Atai Rakhmatov <arahmatov12@gmail.com>
2023-10-07 13:48:43 +0600

    add macros and typedefs to template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -3,10 +3,21 @@ using namespace std;
 
 #define forn(i, a, b) for(ll i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
+#define rall(x) rbegin(x), rend(x)
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(rall(c))
+#define sz(x) x.size()
+#define pb push_back
+#define mp make_pair
 typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<bool> vb;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+
+const ll MOD = 1e9 + 7;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
