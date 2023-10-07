5d0bc4f2a5794a7876531862e7f7a5d37b87c611
Atai Rakhmatov <arahmatov12@gmail.com>
2023-10-07 14:03:02 +0600

    fix typedef issues

--- content/graph/MaximumClique.h
+++ content/graph/MaximumClique.h
@@ -10,12 +10,12 @@
  * faster for sparse graphs.
  * Status: stress-tested
  */
-typedef vector<bitset<200>> vb;
+typedef vector<bitset<200>> vbit;
 struct Maxclique {
-	double limit=0.025, pk=0;
-	struct Vertex { int i, d=0; };
+	double limit = 0.025, pk = 0;
+	struct Vertex { int i, d = 0; };
 	typedef vector<Vertex> vv;
-	vb e;
+	vbit e;
 	vv V;
 	vector<vi> C;
 	vi qmax, q, S, old;
@@ -24,7 +24,7 @@ struct Maxclique {
 		for (auto& v : r) for (auto j : r) v.d += e[v.i][j.i];
 		sort(all(r), [](auto a, auto b) { return a.d > b.d; });
 		int mxD = r[0].d;
-		forn(i,0,sz(r)) r[i].d = min(i, mxD) + 1;
+		forn(i, 0, sz(r)) r[i].d = min(i, mxD) + 1;
 	}
 	void expand(vv& R, int lev = 1) {
 		S[lev] += S[lev - 1] - old[lev];
@@ -33,7 +33,7 @@ struct Maxclique {
 			if (sz(q) + R.back().d <= sz(qmax)) return;
 			q.push_back(R.back().i);
 			vv T;
-			for(auto v:R) if (e[R.back().i][v.i]) T.push_back({v.i});
+			for (auto v : R) if (e[R.back().i][v.i]) T.push_back({ v.i });
 			if (sz(T)) {
 				if (S[lev]++ / ++pk < limit) init(T);
 				int j = 0, mxk = 1, mnk = max(sz(qmax) - sz(q) + 1, 1);
@@ -47,15 +47,16 @@ struct Maxclique {
 					C[k].push_back(v.i);
 				}
 				if (j > 0) T[j - 1].d = 0;
-				forn(k,mnk,mxk + 1) for (int i : C[k])
+				forn(k, mnk, mxk + 1) for (int i : C[k])
 					T[j].i = i, T[j++].d = k;
 				expand(T, lev + 1);
-			} else if (sz(q) > sz(qmax)) qmax = q;
+			}
+			else if (sz(q) > sz(qmax)) qmax = q;
 			q.pop_back(), R.pop_back();
 		}
 	}
 	vi maxClique() { init(V), expand(V); return qmax; }
-	Maxclique(vb conn) : e(conn), C(sz(e)+1), S(sz(C)), old(S) {
-		forn(i,0,sz(e)) V.push_back({i});
+	Maxclique(vbit conn) : e(conn), C(sz(e) + 1), S(sz(C)), old(S) {
+		forn(i, 0, sz(e)) V.push_back({ i });
 	}
 };
