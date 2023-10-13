b506050109cadd729b7af66830bd5b7c797abb29
Atai Rakhmatov <arahmatov12@gmail.com>
2023-10-13 20:34:04 +0600

    Change indentation from spaces to tabs

--- content/number-theory/Fibonacci.h
+++ content/number-theory/Fibonacci.h
@@ -11,20 +11,20 @@ using ull = unsigned ll;
 // for mod and result bigger than int, use modmul
 // from "ModMulLL.h" for all multiplications
 int fib(ull n, int m) {
-    ull a, b, p, q, aq, qq;
-    a = q = 1;
-    b = p = 0;
-    while (n) {
-        if (n & 1) {
-            aq = a*q % m;
-            a = (b*q + aq + a*p) % m;
-            b = (b*p + aq) % m;
-            --n;
-        }
-        qq = q*q % m;
-        q = (2*p*q + qq) % m;
-        p = (p*p + qq) % m;
-        n >>= 1;
-    }
-    return b;
+	ull a, b, p, q, aq, qq;
+	a = q = 1;
+	b = p = 0;
+	while (n) {
+		if (n & 1) {
+			aq = a*q % m;
+			a = (b*q + aq + a*p) % m;
+			b = (b*p + aq) % m;
+			--n;
+		}
+		qq = q*q % m;
+		q = (2*p*q + qq) % m;
+		p = (p*p + qq) % m;
+		n >>= 1;
+	}
+	return b;
 }
